1d935d5b01c7ddfe102b42fe304e7259cd3f833b
remi <remi.petitjean@gmail.com>
2021-03-14 22:50:08 +0000

    time stuff

--- src/stats.rs
+++ src/stats.rs
@@ -1,14 +1,26 @@
 use crossbeam::channel::Receiver;
 use std::io::Result;
+use std::time::{Duration, Instant};
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+    let start = Instant::now();
+    let mut timer = Timer::new();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
-        total_bytes += num_bytes;
 
-        if !silent {
-            eprint!("\r{}", total_bytes);
+        timer.update();
+        let rate_per_second = num_bytes as f64 / (1024. * 1024. * timer.delta.as_secs_f64());
+
+        total_bytes += num_bytes;
+        if !silent && timer.ready {
+            timer.ready = false;
+            eprint!(
+                "\r{} in {} [{:.0}Mb/s]",
+                total_bytes,
+                start.elapsed().as_secs().as_time(),
+                rate_per_second
+            );
         }
         if num_bytes == 0 {
             break;
@@ -18,4 +30,47 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+trait TimeOutput {
+    fn as_time(&self) -> String;
+}
+
+impl TimeOutput for u64 {
+    fn as_time(&self) -> String {
+        let (hours, left) = (*self / 3600, *self % 3600);
+        let (minutes, seconds) = (left / 60, left % 60);
+        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+    }
+}
+
+struct Timer {
+    last_instant: Instant,
+    delta: Duration,
+    period: Duration,
+    countdown: Duration,
+    ready: bool,
+}
+
+impl Timer {
+    fn new() -> Self {
+        let now = Instant::now();
+        Self {
+            last_instant: now,
+            delta: Duration::default(),
+            period: Duration::from_millis(1000),
+            countdown: Duration::default(),
+            ready: true,
+        }
+    }
+
+    fn update(&mut self) {
+        let now = Instant::now();
+        self.delta = now - self.last_instant;
+        self.last_instant = now;
+        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
+            self.ready = true;
+            self.period
+        });
+    }
 }
